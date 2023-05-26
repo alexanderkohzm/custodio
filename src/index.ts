73f23b17b3826983dd617be12bdf172e13ab95dd
alexanderkohzm <127705458+alexanderkohzm@users.noreply.github.com>
2023-05-26 10:08:09 +0800

    added port null check

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT
 
-const startServer = async (app: Express, port: string) => {
+const startServer = async (app: Express, port: string | undefined) => {
 
   const mongoDbURL = process.env.MONGODB_URL!
   connectToMongoDb(mongoDbURL)
 
   app.use('/', router)
+
+  if (!port) {
+    throw new Error(`Please pass in a port number, current port number: ${port}`)
+  }
   app.listen(port, () => {
     console.log(`🚀 Server is running at http://localhost:${port}`)
   })
 }
 
-startServer(app, port!);
+startServer(app, port);
 
 export { app }
